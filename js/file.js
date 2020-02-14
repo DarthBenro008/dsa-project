bec9ab2e47a9239d7cce28f5e3ab5df034261465
parthnamdev <parthnamdev@users.noreply.github.com>
2020-02-14 18:12:27 +0530

    blur_added

--- js/file.js
+++ js/file.js
@@ -1,11 +1,22 @@
 //External JS File
 function callPopUp() {
-    document.getElementById("popup").style.zIndex = "-1";
-    document.getElementById("algorithm").style.display = "block";
+  var elements = document.getElementsByClassName("algorithm");
+  if (elements.length > 0) {
+    elements[0].style.transition = "0.2s";
+    elements[0].style.zIndex = "2";
+    elements[0].style.display = "block";
+    elements[0].style.top = "180px";
     
-  }
-function changeText(){
 
+  }
+  var elements_base_blur = document.getElementsByClassName("top");
+  if (elements_base_blur.length > 0) {
+    elements_base_blur[0].style.filter = "blur(5px)";
+    elements_base_blur[0].style.zIndex = "-1";
+    elements_base_blur[0].style.display = "block";
+    elements_base_blur[0].style.transition = "0.5s";
+  }
+}
+function changeText() {
   document.getElementById("texto").innerHTML = "New text!";
 }
-  
