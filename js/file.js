c116a889fa099d7be01ffb807bbc3665795df5c5
DarthBenro008 <hkpdev008@gmail.com>
2020-02-14 18:47:55 +0530

    NullCheck on Input & Default Checked

--- js/file.js
+++ js/file.js
@@ -1,5 +1,13 @@
 //External JS File
 function callPopUp() {
+  var word = document.getElementById("word_input").value; 
+  var wordNullCheck = 0;
+  console.log(word);
+  if(word ===  ""){
+    alert("Don't try to be supersmart you stinky piece of shit");
+    wordNullCheck= 1;
+  }
+  if(wordNullCheck==0){
   var elements = document.getElementsByClassName("algorithm");
   if (elements.length > 0) {
     elements[0].style.transition = "0.2s";
@@ -9,6 +17,7 @@ function callPopUp() {
     
 
   }
+  document.getElementsByName("algoradio")[0].setAttribute("checked","true"); 
   var elements_base_blur = document.getElementsByClassName("top");
   if (elements_base_blur.length > 0) {
     elements_base_blur[0].style.filter = "blur(5px)";
@@ -16,6 +25,7 @@ function callPopUp() {
     elements_base_blur[0].style.display = "block";
     elements_base_blur[0].style.transition = "0.5s";
   }
+}
 }
 function changeText() {
   document.getElementById("texto").innerHTML = "New text!";
